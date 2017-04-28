6cbae2f48a8a5cec486b73ab0d49771116034120
nelsonic <contact.nelsonic+github@gmail.com>
2017-04-28 15:37:26 +0100

    re-built the Time MVP client in less than an hour: https://github.com/dwyl/time/issues/97#issuecomment-298014177

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ AKA "one to throw away"!
 
 ## Why?
 
-To get _something_ I can start using myself.
+To get _something_ I can start using _myself_.
+
+> This code is not written for anyone `else` so please don't "_judge_" it!
 
 ## What?
 
@@ -17,7 +19,14 @@ as fast as possible!
 
 ## How?
 
-Read the code!
+The code in this project/MVP is meant to be: (_in order of priority_)
++ _**Functional** _
++ _**Human-readable** so **anyone** with basic HTML/JavaScript knowledge
+can read and **understand*** it_.
+
+We are _not_ making _any_ attempt to make the code:
+"_High Performance_", "_Scalable_" or "_Elegant_" in _any_ way.
+We consider the MVP to be "_quick_" but _not_ "_dirty_".
 
 
 
@@ -25,7 +34,7 @@ Read the code!
 
 > _**Q**: **Why JavaScript**?_ <br />
 > _**A**: Because for better-or-worse it's **the language everyone knows**!
-and **annyoningly** it's what I have the most **practice** with.
+and **annyoningly** it's what I have the most **practice** with._
 
 > _**Q**: **Why aren't** you using any "**Frameworks**"?_ <br />
-> _**A**: I am! Read the code!
+> _**A**: I am! Read the code!_
--- client.js
+++ client.js
@@ -1,5 +1,3 @@
-console.log('Welcome to Monotask Timer Land!');
-
 // GLOBAL store for all data
 var store = {};
 
@@ -11,19 +9,13 @@ function initialise_state() {
     if('localStorage' in window && window['localStorage'] !== null
       && localStorage.getItem('store')) {
       store = JSON.parse(localStorage.getItem('store'));
-      console.log(store);
+      // console.log(store);
     } else { // always initialise to empty
-      store = {
-        counter: 1,
-        timers: []
-      };
+      store = { timers: [] };
     }
   } catch(e) {
-    console.log('No Store Saved in localStorage!');
-    store = {
-      counter: 1,
-      timers: []
-    };
+    console.log('No Store Saved in localStorage! Welcome to Timer Land!');
+    store = { timers: [] };
     return false;
   }
 }
@@ -42,25 +34,50 @@ function save_state_to_localstorage() {
 }
 
 /**
- * format timer in MM:SS
+ * format timer in MM:SS (no need for hours, yet!)
  */
-function format_timer(seconds) {
+function format_timer(seconds) { // "pad" with a 00 or 0 if required:
+  // seconds = seconds || 0;
+  if(isNaN(seconds)) {
+    // start_timer();
+    console.log('format_timer(', seconds ,')');
+    seconds = 321;
+  }
   var MM = String("00" + Math.floor(seconds / 60)).slice(-2);
   var SS = String("00" + seconds % 60).slice(-2);
   return MM + ':' + SS;
 }
 
-
-function update_tab_title_counter() {
-  var start_time = get_current_acctive_timer().start;
+/**
+ * Display Timer in Browser Tab Title see: github.com/dwyl/time/issues/37
+ */
+function update_desktop_browser_tab_title_counter() {
+  var start_time = get_current_active_timer().start;
   var now = Date.now();
   var seconds = Math.ceil((now - start_time) / 1000); // round up!
   var default_minutes_per_pomodoro = 25;
   var default_time_estimate = default_minutes_per_pomodoro * 60 * 1000;
   var remaining = Math.floor((start_time + default_time_estimate - now) / 1000);
-  document.title = format_timer(seconds) + ' | ' + format_timer(remaining)
+  var rem = (remaining < 0 ? '-' : '') + format_timer(remaining)
+  document.title = format_timer(seconds) + ' / ' + rem
     + ' (Est: ' + default_minutes_per_pomodoro +' mins)';
 }
+
+// this could be "DRY-ed" out ...
+function update_timer_counter() {
+  var start_time = get_current_active_timer().start;
+  var now = Date.now();
+  var seconds = Math.ceil((now - start_time) / 1000); // round up!
+  var default_minutes_per_pomodoro = 25;
+  var default_time_estimate = default_minutes_per_pomodoro * 60 * 1000;
+  var remaining = Math.floor((start_time + default_time_estimate - now) / 1000);
+  var rem = (remaining < 0 ? '-' : '') + format_timer(remaining)
+  document.getElementById("timer").innerHTML = format_timer(seconds)
+  + ' / ' + rem;
+  document.getElementById("estimate").innerHTML =  '(Estimate: '
+    + default_minutes_per_pomodoro +' minutes)'
+}
+
 
 function timer_is_running() {
   console.log('Timer Count: ' + store.timers.length);
@@ -72,12 +89,11 @@ function timer_is_running() {
   return running.length > 0;
 }
 
-function get_current_acctive_timer() {
+function get_current_active_timer() {
   var running = store.timers.filter(function (timer) {
-    // console.log(timer);
     return !timer.end; // we only care about the timers without an end time!
   });
-  // console.log('Number of Active Timers: ' + running.length);
+  // console.log('get_current_active_timer()', running)
   return running[0];
 }
 
@@ -85,23 +101,90 @@ function get_current_acctive_timer() {
 function start_timer() {
   console.log('start_timer()');
   if (timer_is_running()) {
-
+    // do nothing
+    // console.log(get_current_active_timer());
   }
   else {
     store.timers.push({
       start: Date.now()
     });
+
   }
   save_state_to_localstorage();
 }
 
-
 function clock() {
-  var interval = window.setInterval(function () {
-    update_tab_title_counter();
-  }, 1000);
+  store.interval = setInterval(function () {
+    update_desktop_browser_tab_title_counter();
+    update_timer_counter();
+  }, 2000); // ensure we don't skip seconds in the UI
 }
 
-initialise_state()
-clock();
-start_timer();
+/**
+ * this will delete the state in the app and the localStorage irrevocably!
+ */
+function reset_state() {
+  store = { timers: [] }; // reset
+  localStorage.removeItem('store');
+  window.location.reload();
+  return false;
+}
+
+/**
+ * remove blank lines and excess white space from description
+ * auto grow the height of the text area when text goes on new line
+ */
+function auto_grow(element) {
+  clearInterval(store.interval);  // avoid showing a "NaN" while updating
+  var e = element || document.getElementById("description")
+  e.style.height = "5px";
+  e.style.height = (e.scrollHeight)+"px";
+  e.value.replace(/^\s+|\s+$/g, "");
+  clock();
+} // see: http://stackoverflow.com/questions/17772260/textarea-auto-height
+
+function save_timer_description() {
+  var description = document.getElementById("description").value
+  console.log('save_timer_description() > ' + description)
+  store.timers[store.timers.length-1].description = description;
+}
+
+function stop_timer() {
+  store.timers[store.timers.length-1].end = Date.now();
+  render_complete();
+  start_timer(); // start a new timer cause time doesn't stop!
+  var desc = document.getElementById("description");
+  desc.value = 'Describe your task ...';
+  auto_grow();
+  desc.focus();
+}
+
+function render_complete() {
+  save_timer_description(); // avoid saving a timer without a description.
+
+  // clear the placeholder table
+  var old_tbody = document.getElementById('timers')
+    .getElementsByTagName('tbody')[0]
+  old_tbody.parentNode.replaceChild(document.createElement('tbody'), old_tbody)
+
+  store.timers.forEach(function(t) {
+    if(t.end) {
+      var seconds = Math.floor((t.end - t.start)/1000);
+      // developer.mozilla.org/en-US/docs/Web/API/HTMLTableElement/insertRow
+      var tableRef = document.getElementById('timers')
+        .getElementsByTagName('tbody')[0];
+      var newRow = tableRef.insertRow(0);
+      newRow.className = 'striped--near-white';
+      // Insert a cell in the row at index 0
+      var descCell = newRow.insertCell(0);
+      descCell.className = 'pv2 ph3';
+      var newText = document.createTextNode(t.description);
+      descCell.appendChild(newText);
+
+      var timeCell = newRow.insertCell(1);
+      timeCell.className = 'pv2 ph3 tr';
+      var newText = document.createTextNode(format_timer(seconds));
+      timeCell.appendChild(newText);
+    }
+  });
+}
